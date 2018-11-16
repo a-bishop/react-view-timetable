50fb1a7bd3b9c871d55f30a485bcab8d0c781346
Andrew Bishop <andrew.bishop53@gmail.com>
2018-11-16 14:32:39 -0800

    Updates

---
+++ precache-manifest.1448bf9fe95be820750a65e281f3b7e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fde7a4760a71f157425c",
+    "url": "/a-bishop/react-view-timetable/static/css/main.5fb13a7a.chunk.css"
+  },
+  {
+    "revision": "fde7a4760a71f157425c",
+    "url": "/a-bishop/react-view-timetable/static/js/main.fde7a476.chunk.js"
+  },
+  {
+    "revision": "97e0e8413b751365e1dc",
+    "url": "/a-bishop/react-view-timetable/static/js/1.97e0e841.chunk.js"
+  },
+  {
+    "revision": "e842d3fc719480f5120c",
+    "url": "/a-bishop/react-view-timetable/static/js/runtime~main.e842d3fc.js"
+  },
+  {
+    "revision": "e1374bb4d0b318612a137b02761c379a",
+    "url": "/a-bishop/react-view-timetable/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/a-bishop/react-view-timetable/precache-manifest.1448bf9fe95be820750a65e281f3b7e6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/a-bishop/react-view-timetable/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
