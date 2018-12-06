5165dce757c074730adcf59d2fb4b523672d6db5
Andrew Bishop <andrew.bishop53@gmail.com>
2018-12-06 14:35:47 -0800

    updated default routing

---
+++ build/precache-manifest.ea99d1468a7e179ddd059aec4ab84d6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9be0fce2718f0bfa810",
+    "url": "/react-view-timetable/static/css/main.90db22cb.chunk.css"
+  },
+  {
+    "revision": "f9be0fce2718f0bfa810",
+    "url": "/react-view-timetable/static/js/main.f9be0fce.chunk.js"
+  },
+  {
+    "revision": "b3d48f9645bdcc8fa592",
+    "url": "/react-view-timetable/static/js/1.b3d48f96.chunk.js"
+  },
+  {
+    "revision": "adc6010dcf2fa22b0987",
+    "url": "/react-view-timetable/static/js/runtime~main.adc6010d.js"
+  },
+  {
+    "revision": "10318451320992e8e070f8edd4100b33",
+    "url": "/react-view-timetable/index.html"
+  }
+];
