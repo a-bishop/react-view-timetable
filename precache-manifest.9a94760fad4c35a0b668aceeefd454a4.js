2452d36406b2efe5aa802361946c541b1d621fd7
Andrew Bishop <andrew.bishop53@gmail.com>
2019-01-22 18:30:01 -0800

    Updates

---
+++ precache-manifest.9a94760fad4c35a0b668aceeefd454a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adc6010dcf2fa22b0987",
+    "url": "/react-view-timetable/static/js/runtime~main.adc6010d.js"
+  },
+  {
+    "revision": "6dc7c6e669ecf3b5bfc8",
+    "url": "/react-view-timetable/static/js/main.6dc7c6e6.chunk.js"
+  },
+  {
+    "revision": "5c3ac0d018168a40872f",
+    "url": "/react-view-timetable/static/js/1.5c3ac0d0.chunk.js"
+  },
+  {
+    "revision": "6dc7c6e669ecf3b5bfc8",
+    "url": "/react-view-timetable/static/css/main.2edddcd7.chunk.css"
+  },
+  {
+    "revision": "8ab3427169a59af732c9a7d29f819a3c",
+    "url": "/react-view-timetable/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-view-timetable/precache-manifest.3702bba2cac7073f4886857ba310e895.js"
+  "/react-view-timetable/precache-manifest.9a94760fad4c35a0b668aceeefd454a4.js"
 );
 
 workbox.clientsClaim();
